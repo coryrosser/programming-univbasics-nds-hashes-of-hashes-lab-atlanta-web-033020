3998cbbe48f3d825b90e5980a0719e352ac6802d
Cortlyn Rosser <coryrosser@gmail.com>
2020-03-23 16:19:06 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,19 +46,16 @@ def naming_system
                label: "Genus",
                sub_category {
                label: "Species"
-               }}}}
+               }
+               
+             }
+             
+           }
+           
          }
-  }
+      }
   
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
+   }
+}
 
 end
